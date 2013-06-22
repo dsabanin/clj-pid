25419b77ed396d457ee3762f8d66f752a8fb03d9
Dima Sabanin <sdmitry@gmail.com>
2013-06-22 12:45:51 -0400

    Moving Clojure to dev dependencies, version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pid "0.1"
+(defproject clj-pid "0.1.1"
   :description "Tiny library to get current process PID and handle the pid files"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[lein-clojars "0.9.0"]])
+  :dev-dependencies [[lein-clojars "0.9.0"]
+                     [org.clojure/clojure "1.5.1"]])
